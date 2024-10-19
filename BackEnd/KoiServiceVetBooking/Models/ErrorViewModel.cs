d042ac0b974ed94b576d6c33eed33f395d55e29a
nalunglinh <121504949+nalunglinh@users.noreply.github.com>
2024-10-20 01:08:05 +0700

    Delete BackEnd directory

--- BackEnd/KoiServiceVetBooking/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KoiServiceVetBooking.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
