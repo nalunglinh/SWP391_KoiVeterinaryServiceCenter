b6525ec6729c8c5dea698c17d4d9e54215470c1d
nalunglinh <nhatanhok555@gmail.com>
2024-11-07 12:29:45 +0700

    update all

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,25 +1,25 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Ensure you import BrowserRouter
-import HomePage from './Pages/HomePage';
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
-import StandardCare from './Pages/StandardCare';
-import AquaticPlant from './Pages/AquaticPlant';
-import KoiFood from './Pages/KoiFood';
-import KoiLifespan from './Pages/KoiLifespan';
-import Contact from './Pages/Contact';
-import FAQPage from './Pages/FAQ';
-import ForgotPassword from './Pages/ForgotPassword';
-import ResetPassword from './Pages/ResetPassword';
-import Profile from './Pages/Profile';
-import EditProfile from './Pages/EditProfile';
-import Services from './Pages/Services';
+import HomePage from './Pages/HomePage.jsx';
+import SignIn from './Pages/SignIn.jsx';
+import SignUp from './Pages/SignUp.jsx';
+import StandardCare from './Pages/StandardCare.jsx';
+import AquaticPlant from './Pages/AquaticPlant.jsx';
+import KoiFood from './Pages/KoiFood.jsx';
+import KoiLifespan from './Pages/KoiLifespan.jsx';
+import Contact from './Pages/Contact.jsx';
+import FAQPage from './Pages/FAQ.jsx';
+import ForgotPassword from './Pages/ForgotPassword.jsx';
+import ResetPassword from './Pages/ResetPassword.jsx';
+import Profile from './Pages/Profile.jsx';
+import EditProfile from './Pages/EditProfile.jsx';
+import Services from './Pages/Services.jsx';
 import Pond from './Pages/Pond&Treatment.jsx';
-import Consultation from './Pages/Consultation';
-import ProfileDoctor from './Pages/ProfileDoctor';
+import Consultation from './Pages/Consultation.jsx';
+import ProfileDoctor from './Pages/ProfileDoctor.jsx';
 import Consultpayment from './Pages/Consultpayment.jsx';
 import Payment from './Pages/Tpayment.jsx';
-import History from './Pages/History';
+import History from './Pages/History.jsx';
 
 
 
